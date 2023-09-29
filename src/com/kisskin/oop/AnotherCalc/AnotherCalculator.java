2bb52ad6fa8757684475fe0f0fc4fc2d06c63753
Kisskin-Mister <143715327+Kisskin-Mister@users.noreply.github.com>
2023-09-30 02:51:33 +0300

    Исправленны ошибки

--- src/com/kisskin/oop/AnotherCalc/AnotherCalculator.java
+++ src/com/kisskin/oop/AnotherCalc/AnotherCalculator.java
@@ -15,17 +15,26 @@ public class AnotherCalculator {
         do {
             Process.process();
             Scanner sc = new Scanner(System.in);
-            if (isNumber(Process.value1) == isNumber(Process.value2)) {
+            if ((isNumber(Process.value1) == isNumber(Process.value2))){
                 int digit1 = conv.finalConv(Process.value1);
                 int digit2 = conv.finalConv(Process.value2);
+                if ((ReturningValue.functionArabResult(digit1, digit2, Process.func)) < 0){
+                    System.out.println("Calculator works only with positive values. Try again!");
+                    System.out.println("Want to continue calculating? [Type Y for yes / Type any or Enter for exit]");
+                    String negative = new String("Y");
+                    answer = sc.nextLine().toUpperCase();
+                }else{
                 System.out.println(Process.value1.toUpperCase() + " " + Process.func + " " + Process.value2.toUpperCase() + " = " + ReturningValue.functionResult(digit1, digit2, Process.func));
-                System.out.println("Want to end calculating?[Y/N]");
-                answer = sc.nextLine().toUpperCase();
+                System.out.println("Want to continue calculating? [Type Y for yes / Type any or Enter for exit]");
+                String negative = new String("Y");
+                answer = sc.nextLine().toUpperCase();}
             } else {
-                System.out.println("БАЛЯ ЦИФРЫ ДОЛЖНЫ БЫТЬ ОДНОГО РЕГИСТРА. ДАВАЙ ЗАНОВО");
-                Process.process();
+                System.out.println("Incompatible data types. Try again:)");
+                System.out.println("Want to continue calculating? [Type Y for yes / Type any or Enter for exit]");
+                String negative = new String("Y");
+                answer = sc.nextLine().toUpperCase();
             }
-        } while (answer.equals("N"));
+        } while (answer.equals("Y"));
 
     }
 }
--- src/com/kisskin/oop/AnotherCalc/Process.java
+++ src/com/kisskin/oop/AnotherCalc/Process.java
@@ -12,28 +12,33 @@ public class Process {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number");
         value1 = sc.nextLine();
-        if(conv.finalConv(value1) == 0){
+        if(conv.finalConv(value1) <= 0){
             do{
                 System.out.println("Error! Enter the first number from 1 to 10 or from I to X");
                 value1 = sc.nextLine();
-            }while(conv.finalConv(value1) != 0);
+            }while(conv.finalConv(value1) <= 0);
         }
         System.out.println("Enter the function (only + - * or /)");
         func = sc.nextLine();
-        if ((!func.equals("+") || !func.equals("-") || !func.equals("*") || !func.equals("/"))) {
+        String funcPlus = new String("+");
+        String funcMinus = new String("-");
+        String funcMulti = new String("*");
+        String funcDivide = new String("/");
+        if (!(func.equals("+") || func.equals("-") || func.equals("*") || func.equals("/")))
+        {
             do {
-                System.out.println("БАЛЯ НЕПРАВИЛЬНАЯ ФУНКЦИЯ");
+                System.out.println("Incorrect function (Be sure you're not typing Space)");
                 System.out.println("Enter the function (only + - * or /)");
                 func = sc.nextLine();
             }while (!(func.equals("+") || func.equals("-") || func.equals("*") || func.equals("/")));
         }
         System.out.println("Enter the second number");
         value2 = sc.nextLine();
-        if(conv.finalConv(value2) == 0){
+        if(conv.finalConv(value2) <= 0){
             do{
                 System.out.println("Error! Enter the second number from 1 to 10 or from I to X");
                 value2 = sc.nextLine();
-            }while(conv.finalConv(value2) != 0);
+            }while(conv.finalConv(value2) <= 0);
         }
     }
 }
